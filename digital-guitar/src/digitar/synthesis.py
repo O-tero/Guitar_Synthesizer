a2a227b05f0b2cf022064b7fc0b94b0258c4c5ec
Otero <oteronickbrian@gmail.com>
2024-07-19 02:53:41 +0300

    Remove the DC Bias

--- digital-guitar/src/digitar/synthesis.py
+++ digital-guitar/src/digitar/synthesis.py
@@ -1,5 +1,12 @@
 from dataclasses import dataclass
+from itertools import cycle
+from typing import Iterator
+
+import numpy as np
+
 from digitar.burst import BurstGenerator, WhiteNoise
+from digitar.processing import normalize, remove_dc
+from digitar.temporal import Hertz, Time
 
 AUDIO_CD_SAMPLING_RATE = 44100
 
@@ -7,3 +14,34 @@ AUDIO_CD_SAMPLING_RATE = 44100
 class Synthesizer:
     burst_generator: BurstGenerator = WhiteNoise()
     sampling_rate: int = AUDIO_CD_SAMPLING_RATE
+
+    def vibrate(
+        self, frequency: Hertz, duration: Time, damping: float = 0.5
+    ) -> np.ndarray:
+        assert 0 < damping <= 0.5
+        return normalize(
+            remove_dc(
+                np.fromiter(
+                    feedback_loop(),
+                    np.float64,
+                    duration.get_num_samples(self.sampling_rate),
+                )
+            )
+        )
+
+        def feedback_loop() -> Iterator[float]:
+            buffer = self.burst_generator(
+                num_samples=round(self.sampling_rate / frequency),
+                sampling_rate=self.sampling_rate
+            )
+            for i in cycle(range(buffer.size)):
+                yield (current_sample := buffer[i])
+                next_sample = buffer[(i + 1) % buffer.size]
+                buffer[i] = (current_sample + next_sample) * damping
+                
+       return np.fromiter(
+            feedback_loop(),
+            np.float64,
+            duration.get_num_samples(self.sampling_rate),
+        )
+       
