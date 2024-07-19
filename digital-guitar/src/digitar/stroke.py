a8ba1c4785346fb54e95c9b3fed55a5a6fa8d676
Otero <oteronickbrian@gmail.com>
2024-07-20 00:25:36 +0300

    adjust the stroke speed

---
+++ digital-guitar/src/digitar/stroke.py
@@ -0,0 +1,23 @@
+import enum
+from dataclasses import dataclass
+from typing import self
+
+from digitar.temporal import Time
+
+class Direction(enum.Enum):
+    DOWN = enum.auto()
+    UP = enum.auto()
+    
+@dataclass(frozen=True)
+class Velocity:
+    direction: Direction
+    delay: Time
+    
+    @classmethod
+    def down(cls, delay:Time) -> Slef:
+        return cls(Direction.DOWN, delay)
+    
+    @classmethod
+    def up(cls, delay: Time) -> Self:
+        return cls(Direction.UP, delay)
+    
