215749422f29aa55b72a5b6436ffc811ef305c58
Otero <oteronickbrian@gmail.com>
2024-07-25 13:29:27 +0300

    implement the guitar tab reader

--- digital-guitar/tablature/player.py
+++ digital-guitar/tablature/player.py
@@ -3,12 +3,17 @@ from fractions import Fraction
 from pathlib import Path
 
 import numpy as np
+import pedalboard
+
+
 from digitar.chord import Chord
 from digitar.instrument import PluckedStringInstrument, StringTuning
+from digitar.processing import normalize
 from digitar.stroke import Velocity
 from digitar.synthesis import Synthesizer
 from digitar.temporal import MeasuredTimeline, Time
 from digitar.track import AudioTrack
+from pedalboard.io import AudioFile
 
 from tablature import models
 
@@ -28,7 +33,29 @@ def parse_args() -> Namespace:
 
 def play(args: Namespace) -> None:
     song = models.Song.from_file(args.path)
-    tracks = [track.weight * synthesize(track) for track in song.tracks.values()]
+    samples = normalize(
+        np.sum(
+            pad_to_longest(
+                [track.weight * synthesize(track) for track in song.tracks.values()]
+            ),
+            axis=0,
+        )
+    )
+    save(
+        samples,
+        args.output or Path.cwd() / args.path.with_suffix(".mp3").name,
+    )
+
+
+def pad_to_longest(tracks: list[np.ndarray]) -> list[np.ndarray]:
+    max_length = max(array.size for array in tracks)
+    return [np.pad(array, (0, max_length - array.size)) for array in tracks]
+
+
+def save(samples: np.ndarray, path: Path) -> None:
+    with AudioFile(str(path), "w", SAMPLING_RATE) as file:
+        file.write(samples)
+    print(f"Saved file {path.absolute()}")
 
 
 def synthesize(track: models.Track) -> np.ndarray:
@@ -67,3 +94,21 @@ def read(
                 ),
             )
         next(timeline)
+
+
+def apply_effects(audio_track: AudioTrack, instrument: models.Instrument) -> np.ndarray:
+    effects = pedalboard.Pedalboard(get_plugins(instrument))
+    return effects(audio_track.samples, audio_track.sampling_rate)
+
+
+def get_plugins(instrument: models.Instrument) -> list[pedalboard.Plugin]:
+    return [get_plugin(effect) for effect in instrument.effects]
+
+
+def get_plugin(effect: str | dict) -> pedalboard.Plugin:
+    match effect:
+        case str() as class_name:
+            return getattr(pedalboard, class_name)()
+        case dict() as plugin_dict if len(plugin_dict) == 1:
+            class_name, params = list(plugin_dict.items())[0]
+            return getattr(pedalboard, class_name)(**params)
